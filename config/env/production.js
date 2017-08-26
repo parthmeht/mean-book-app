0d3a22022060f3675a6e18c014e86d9cb57d73e0
Parth Mehta <parthmeht@gmail.com>
2017-08-26 16:51:48 +0530

    google auth changes

--- config/env/production.js
+++ config/env/production.js
@@ -10,5 +10,10 @@ module.exports = {
         clientID: 'gDEWOh8pgaGXFP429kRo30zOM',
         clientSecret: '4I3FLVJLnHSIxmYF3BUsqzFZDNkVJi9sGn2czUwZr2G8d8WILD',
         callbackURL: 'https://mean-book-app.herokuapp.com/oauth/twitter/callback'
+    },
+    google: {
+        clientID: '818773428299-4c7l7a7bdg5hjc8hjrk76othvndjq3sj.apps.googleusercontent.com',
+        clientSecret: 'ZWPl-iSDndTU33dTVNz-rZnU',
+        callbackURL: 'https://mean-book-app.herokuapp.com/oauth/google/callback'
     }
 };
