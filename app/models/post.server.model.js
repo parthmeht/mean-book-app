d3d9515156745caeaf1e632305e89019dbb9f729
Parth Mehta <parthmeht@gmail.com>
2017-08-26 14:42:52 +0530

    first commit

---
+++ app/models/post.server.model.js
@@ -0,0 +1,16 @@
+var PostSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: Schema.ObjectId,
+      ref: 'User'
+    }
+});
+  
+mongoose.model('Post', PostSchema);
---
+++ config/env/development.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: 'mongodb://admin:admin@ds021356.mlab.com:21356/northwindnode',
+    sessionSecret: 'developmentSessionSecret',
+    facebook: {
+        clientID: '541523029512626',
+        clientSecret: '875501c5e622268479129f6d8945e89c',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    }
+};
