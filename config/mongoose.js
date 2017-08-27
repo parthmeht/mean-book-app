867a3567340f6efd80552a4ce2496fcfd4a4cd5b
Parth Mehta <parthmeht@gmail.com>
2017-08-27 17:29:26 +0530

    Commens inclusion

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,18 @@
-var config = require('./config'),
-mongoose = require('mongoose');
+// Invoke 'strict' JavaScript mode
+'use strict';
 
+// Load the module dependencies
+var	config = require('./config'),
+	mongoose = require('mongoose');
+
+// Define the Mongoose configuration method
 module.exports = function() {
-    var db = mongoose.connect(config.db);
+	// Use Mongoose to connect to MongoDB
+	var db = mongoose.connect(config.db);
 
-    require('../app/models/user.server.model');
+	// Load the 'User' model 
+	require('../app/models/user.server.model');
 
-    return db;
+	// Return the Mongoose connection instance
+	return db;
 };
