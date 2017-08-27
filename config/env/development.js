8f81d0e14ace0987f737c7553e544381328edfbf
Parth Mehta <parthmeht@gmail.com>
2017-08-27 17:24:57 +0530

    Angular Introduction - end of chapter 7

--- config/env/development.js
+++ config/env/development.js
@@ -5,5 +5,15 @@ module.exports = {
         clientID: '541523029512626',
         clientSecret: '875501c5e622268479129f6d8945e89c',
         callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'gDEWOh8pgaGXFP429kRo30zOM',
+        clientSecret: '4I3FLVJLnHSIxmYF3BUsqzFZDNkVJi9sGn2czUwZr2G8d8WILD',
+        callbackURL: 'https://mean-book-app.herokuapp.com/oauth/twitter/callback'
+    },
+    google: {
+        clientID: '818773428299-4c7l7a7bdg5hjc8hjrk76othvndjq3sj.apps.googleusercontent.com',
+        clientSecret: 'ZWPl-iSDndTU33dTVNz-rZnU',
+        callbackURL: 'https://mean-book-app.herokuapp.com/oauth/google/callback'
     }
 };
