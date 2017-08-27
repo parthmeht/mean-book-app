c4a3d34ff33bfba47b2e2d8c5fc7024ab99266b5
Parth Mehta <parthmeht@gmail.com>
2017-08-27 17:27:24 +0530

    Comments added

--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -1,5 +1,10 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Create the 'example' controller
 angular.module('example').controller('ExampleController', ['$scope', 'Authentication',
-  function($scope, Authentication) {
-    $scope.name = Authentication.user ? Authentication.user.fullName : 'MEAN Application';
-  }
+	function($scope, Authentication) {
+		// Get the user's 'fullName' 
+		$scope.name = Authentication.user ? Authentication.user.fullName : 'MEAN Application';
+	}
 ]);
